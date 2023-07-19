a8665396e7130f3ea9658650ab676315aa7fbfea
Henrique Salm Prim <henrique_salmprim@hotmail.com>
2023-07-19 11:00:32 -0300

    refactor/fix: timestamps and user key in schema

--- Models/Message.js
+++ Models/Message.js
@@ -2,10 +2,13 @@ const mongoose = require('mongoose');
 
 const MessageSchema = new mongoose.Schema({
     chat_id: { type: String, required: true },
-    user_id: { type: String, required: true },
+    username: { type: String, required: true },
     message: { type: String, required: true },
-    timestamp: { type: Date, required: true },
-});
+}, {
+    timestamps: true,
+    get: time => time.toDateString()
+ }
+);
    
 const Message = mongoose.model('Message', MessageSchema);
 
