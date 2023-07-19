387929d7e2059afb35fa3018d5a1a725775ef08b
Henrique Salm Prim <henrique_salmprim@hotmail.com>
2023-07-19 11:07:43 -0300

    feat/fix: adds cors, changes port and user route

--- index.js
+++ index.js
@@ -2,15 +2,19 @@ const express = require('express');
 const connect_DB = require('./Models/connection');
 const ErrorHandler = require('./Middlewares/ErrorHandler');
 const UserRoutes = require('./Routes/UserRoutes');
+const ChatRoute = require('./Routes/ChatRoute');
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(ErrorHandler);
 
-app.use('/user', UserRoutes);
+app.use('/', UserRoutes);
+app.use('/chat', ChatRoute);
 
 connect_DB()
-    .then(() => app.listen(3000, () => console.log('listening at 3000')))
+    .then(() => app.listen(3001, () => console.log('listening at 3001')))
     .catch((err) => console.log(err));
 
 
