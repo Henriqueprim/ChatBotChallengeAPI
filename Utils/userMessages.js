8277723642a78c23e01930f9e40df6edbdfe3fc6
Henrique Salm Prim <henrique_salmprim@hotmail.com>
2023-07-19 11:02:38 -0300

    feat: creates message terms file

---
+++ Utils/userMessages.js
@@ -0,0 +1,4 @@
+const greetingsTerms = ['hi', 'hello', 'hey', 'good morning', 'good afternoon', 'good evening'];
+const goodbyeTerms = ['goodbye', 'bye', 'have a nice day'];
+
+module.exports = { greetingsTerms, goodbyeTerms };
